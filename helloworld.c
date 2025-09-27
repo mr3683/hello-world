5a2494ee214a96e86c9c43ba77b3fca31dc172e8
Monse Rosales <shellcatmr@hwsrv-1298717.hostwindsdns.com>
2025-09-27 06:09:45 +0000

    Resolved merge conflict in helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,8 @@
-git add helloworld.c
-git commit -m "Updated hello world"
-git log --oneline
+<<<<<<< HEAD
+printf("Hello world!\n");
+=======
+printf("hello world\n");
+>>>>>>> helloworld
 
 <<<<<<< HEAD
 int main(void)
