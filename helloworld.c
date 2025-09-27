34db9d884e37cd5df3d8cc1f30a5134e6424fa95
Monse Rosales <shellcatmr@hwsrv-1298717.hostwindsdns.com>
2025-09-27 05:51:26 +0000

    Added comment to helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
+git add helloworld.c
+git commit -m "Updated hello world"
+git log --oneline
 
+<<<<<<< HEAD
 int main(void)
 {
   printf("Hello, World!\n");
   return 0;
 }
+=======
+>>>>>>> Added comment to helloworld.c
